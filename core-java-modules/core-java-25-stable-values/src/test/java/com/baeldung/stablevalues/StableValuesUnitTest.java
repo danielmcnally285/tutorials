4b014df5289062c910099eec7d99dff92193c623
danielmcnally285 <144589379+danielmcnally285@users.noreply.github.com>
2025-09-02 16:02:50 +0000

    Add further unit tests

--- core-java-modules/core-java-25-stable-values/src/test/java/com/baeldung/stablevalues/StableValuesUnitTest.java
+++ core-java-modules/core-java-25-stable-values/src/test/java/com/baeldung/stablevalues/StableValuesUnitTest.java
@@ -4,14 +4,69 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+
+import org.junit.jupiter.api.BeforeEach
 
 public class StableValuesUnitTest {
 
+    private Set<String> cities;
+
+    private String expensiveMethodToDetermineCountry(String city) {
+        switch(city) {
+        case "Paris":
+            return "France";
+        case "London":
+            return "England";
+        case "Madrid":
+            return "Spain";
+        default:
+            throw new RuntimeException("Unsupported city");
+        }
+    }
+
     @Test
-    void testStableList() {
+    void givenStableFunctionForCityToCountry_thenVerifyElementsAreExpected() {
+        final Function<String, String> cityToCountry = StableValue.function(cities, city -> expensiveMethodToDetermineCountry(city));
+        assertThat(cityToCountry.apply("Paris")).isEqualTo("France");
+        assertThat(cityToCountry.apply("London")).isEqualTo("England");
+        assertThat(cityToCountry.apply("Madrid")).isEqualTo("Spain");
+    }
+
+    @Test
+    void givenStableFunctionForCityToCountry_whenInvalidInputUsed_thenExceptionThrown() {
+        final Function<String, String> cityToCountry = StableValue.function(cities, city -> expensiveMethodToDetermineCountry(city));
+        assertThatIllegalArgumentException().isThrownBy(() -> cityToCountry.apply("Berlin"));
+    }
+
+    @Test
+    void givenStableListForFiveTimesTable_thenVerifyElementsAreExpected() {
         final List<Integer> fiveTimesTable = StableValue.list(11, index -> index * 5);
         assertThat(fiveTimesTable.get(0)).isEqualTo(0);
         assertThat(fiveTimesTable.get(1)).isEqualTo(5);
+        assertThat(fiveTimesTable.get(2)).isEqualTo(10);
+        assertThat(fiveTimesTable.get(3)).isEqualTo(15);
+        assertThat(fiveTimesTable.get(4)).isEqualTo(20);
+        assertThat(fiveTimesTable.get(5)).isEqualTo(25);
+        assertThat(fiveTimesTable.get(6)).isEqualTo(30);
+        assertThat(fiveTimesTable.get(7)).isEqualTo(35);
+        assertThat(fiveTimesTable.get(8)).isEqualTo(40);
+        assertThat(fiveTimesTable.get(9)).isEqualTo(45);
         assertThat(fiveTimesTable.get(10)).isEqualTo(50);
+    }
+
+    @Test
+    void givenStableMapForCityToCountry_thenVerifyElementsAreExpected() {
+        final Map<String, String> cityToCountry = StableValue.map(cities, city -> expensiveMethodToDetermineCountry(city));
+        assertThat(cityToCountry.get("Paris")).isEqualTo("France");
+        assertThat(cityToCountry.get("London")).isEqualTo("England");
+        assertThat(cityToCountry.get("Madrid")).isEqualTo("Spain");
+    }
+
+    @BeforeEach
+    void init() {
+        cities = Set.of("Paris", "London", "Madrid");
     }
 }
